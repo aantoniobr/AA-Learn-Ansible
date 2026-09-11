421ca6e7f2f4cc8979295a83fe439bcc9967f384
aantoniobr <88464423+aantoniobr@users.noreply.github.com>
2026-09-10 21:53:44 -0300

    Update AutisticPirate.py

--- AutisticPirate.py
+++ AutisticPirate.py
@@ -1,1 +1,19 @@
+from dotenv import load_dotenv
+import discord
+import os
+#load environment variables from .env file
+load_dotenv()
+#set up intents
+intents.message_contents = True # Ensure that your bot can read message content
+client = discord.Client(intents=intents)
+@client.event 
+async def On-ready():
+  print('We have logged in as {0.user}'.format(client))
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  if message.content.startswith('$hello'):
+    await message.channel.send('Hello!')
+client.run(os.getenv('TOKEN'))
 
